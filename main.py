9493df8ef122d265c9279008f9bd3cf72bdf0e01
cshong.gcc@hotmail.com <cshong.gcc@hotmail.com>
2018-01-15 22:17:32 +0800

    Read font files from a list

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from PIL import Image, ImageFont, ImageDraw, ImageOps
 import os
+from PIL import Image, ImageFont, ImageDraw, ImageOps
 
 font_size = 64
-font = ImageFont.truetype("simkai.ttf", font_size)
 
 start = 0x4e00
 end = 0x62ff
@@ -11,11 +10,15 @@ total = end - start
 if(not os.path.isdir("output")):
     os.mkdir("output")
 
-for i in range(start, end + 1):
-    c = chr(i)
-    print("Generating " + c)
-    im = Image.Image()._new(font.getmask(c))
-    #im = im.resize((font_size, font_size), Image.ANTIALIAS)
-    im = ImageOps.invert(im)
-    im.save("output/" + c + ".png")
-    
+fonts = ["simkai.ttf", "simhei.ttf", "simfang.ttf"]
+
+for f in fonts:
+    font = ImageFont.truetype(f, font_size)
+    for i in range(start, end + 1):
+        c = chr(i)
+        print("Generating " + f.split(".")[0] + "_" + c)
+        im = Image.Image()._new(font.getmask(c))
+        #im = im.resize((font_size, font_size), Image.ANTIALIAS)
+        im = ImageOps.invert(im)
+        im.save("output/" + f.split(".")[0] + "_" + c + ".png")
+   
